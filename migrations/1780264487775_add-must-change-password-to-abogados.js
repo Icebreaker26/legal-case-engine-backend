e6a4438dc45ce20b308764e0df6c07789438ca91
Icebreaker26 <alejandro.torres0826@gmail.com>
2026-05-31 21:24:13 -0500

    feat/reset de contraseñas1

---
+++ migrations/1780264487775_add-must-change-password-to-abogados.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.addColumns('abogados', {
+        must_change_password: { type: 'boolean', default: false, notNull: true }
+    }, { ifNotExists: true });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropColumns('abogados', ['must_change_password']);
+};
